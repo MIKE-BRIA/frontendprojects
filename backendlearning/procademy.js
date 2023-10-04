1dc784871e6cd3c9c9301fc644b4543292beeed0
Brian Michael <brianmichaeladero@gmail.com>
2023-10-04 09:22:23 +0300

    js

--- backendlearning/procademy.js
+++ backendlearning/procademy.js
@@ -41,6 +41,10 @@ fs.readFile("./files/start.txt", "utf-8", (error1, data1) => {
 
   fs.readFile(`./files/${data1}.txt`, `utf-8`, (error2, data2) => {
     console.log(data2);
+
+    fs.readFile(`./files/append.txt`, `utf-8`, (error3, data3) => {
+      console.log(data3);
+    });
   });
 });
 
