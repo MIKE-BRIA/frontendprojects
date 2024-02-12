9d3ab72a534194d64017cf527e2926c70e5e574b
Brian Michael <brianmichaeladero@gmail.com>
2024-02-12 11:14:12 +0300

    styling

--- todolist/app.js
+++ todolist/app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const addbutton = document.querySelector(".addbutton");
 const todoList = document.querySelector(".list");
+const filterbutton = document.querySelector(".filterbutton");
 
 //* event listeners
 addbutton.addEventListener("click", addTodo);
 todoList.addEventListener("click", checkTodo);
+filterbutton.addEventListener("click", filterTodo);
 
 //* functions
 
@@ -54,4 +56,31 @@ function checkTodo(event) {
   if (event.target.classList.contains("checkbtn")) {
     Lists.classList.toggle("completed");
   }
+}
+
+function filterTodo(event) {
+  //accessing the filter
+  const todos = todoList.childNodes;
+
+  todos.forEach(function (todo) {
+    switch (event.target.value.toLowerCase()) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
