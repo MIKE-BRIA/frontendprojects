a7517ec6e3785eef90a6f27899f4ab6754e0d526
Brian Michael <brianmichaeladero@gmail.com>
2024-02-28 13:42:32 +0300

    modal dom manipulation

--- modal/script.js
+++ modal/script.js
@@ -1,3 +1,33 @@
-'use strict';
+"use strict";
 
-const 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnClosemodal = document.querySelector(".close-modal");
+const btnsOpenmodal = document.querySelectorAll(".show-modal");
+
+// console.log(btnsOpenmodal);
+
+function addhidden() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+function removehidden() {
+  //   console.log("hey am good");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnsOpenmodal.length; i++) {
+  btnsOpenmodal[i].addEventListener("click", removehidden);
+
+  btnClosemodal.addEventListener("click", addhidden);
+
+  overlay.addEventListener("click", addhidden);
+}
+
+document.addEventListener("keydown", e => {
+  if (e.key === "Escape") {
+    addhidden();
+  }
+});
