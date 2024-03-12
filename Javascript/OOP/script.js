634c0cce19298a2737f47cc83891fc4523a962d0
Brian Michael <brianmichaeladero@gmail.com>
2024-03-12 23:04:53 +0300

    object oriented programming

--- Javascript/OOP/script.js
+++ Javascript/OOP/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+//! Constructor function
+
+//arrow functions don't work for because they don't have the this key word
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const Michael = new Person("Michael", 2002);
+console.log(Michael);
+
+const Anita = new Person("Anita", 2004);
+console.log(Anita);
+
+//! Prototypes
