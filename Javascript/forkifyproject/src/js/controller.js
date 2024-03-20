21ddb1a57cbcd15d4dab27331878a535455b3d4d
Brian Michael <brianmichaeladero@gmail.com>
2024-03-20 23:48:35 +0300

    route controller

--- Javascript/forkifyproject/src/js/controller.js
+++ Javascript/forkifyproject/src/js/controller.js
@@ -1,13 +1,44 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+// console.log(icons);
+
+const recipeContainer = document.querySelector(".recipe");
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    //*loading recipe
+    await model.loadRecipe(id);
+    const recipe = model.state.recipe;
+
+    //* Rendering recipe
+
+    recipeView.render(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+// controlRecipe();
+
+["hashchange", "load"].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
+// window.addEventListener("hashchange", controlRecipe);
+// window.addEventListener("load", controlRecipe);
+console.log("I am arround");
+console.log("helloo");
