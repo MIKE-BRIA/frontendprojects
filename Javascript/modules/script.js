614187b65388f1ebfdbb560480d531c6ff60e3ed
Brian Michael <brianmichaeladero@gmail.com>
2024-03-20 12:26:32 +0300

    modules, tools

---
+++ Javascript/modules/script.js
@@ -0,0 +1,21 @@
+//! Importing module
+
+// import {
+//   addToCart,
+//   totalPrice as price,
+//   totalQuantity,
+// } from "./shoppingCart.js";
+
+// addToCart("sugar", 56);
+// console.log(price, totalQuantity);
+
+// import * as ShoppingCart from "./shoppingCart.js";
+
+// console.log("Importing module");
+
+// console.log(ShoppingCart);
+// ShoppingCart.addToCart("in-ear monitor", 56);
+
+import add from "./shoppingCart.js";
+
+add("pen", 6);
