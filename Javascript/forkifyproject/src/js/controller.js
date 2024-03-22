0fced4d97a54c7c6c875dc6e6f0feb575cf0a348
Brian Michael <brianmichaeladero@gmail.com>
2024-03-22 15:13:21 +0300

    controller

--- Javascript/forkifyproject/src/js/controller.js
+++ Javascript/forkifyproject/src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
 import paginationView from "./views/paginationView.js";
 import bookmarksView from "./views/bookmarksView.js";
+import addRecipeView from "./views/addRecipeView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -105,6 +106,19 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+//*function that handle adding of recipe
+
+const controlAddRecipe = async function (newRecipe) {
+  // console.log(newRecipe);
+  try {
+    //Upload new Recipe data
+    await model.uploadRecipe(newRecipe);
+  } catch (err) {
+    console.error("🤯", err);
+    addRecipeView.renderError(err.message);
+  }
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipe);
@@ -112,6 +126,7 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 
 init();
