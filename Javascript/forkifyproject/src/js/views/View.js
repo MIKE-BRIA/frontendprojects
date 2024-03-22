96dbeaa3f84ed48acefb8d84b82c8da9b578dddc
Brian Michael <brianmichaeladero@gmail.com>
2024-03-22 13:31:44 +0300

    Views

--- Javascript/forkifyproject/src/js/views/View.js
+++ Javascript/forkifyproject/src/js/views/View.js
@@ -3,15 +3,57 @@ import icons from "url:../../img/icons.svg";
 export default class View {
   _data;
 
-  render(data) {
+  render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0)) {
       this.renderError();
       return;
     }
     this._data = data;
     const markup = this._generateMarkup();
+
+    if (!render) return markup;
+
     this._clear();
     this._ParentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  update(data) {
+    // if (!data || (Array.isArray(data) && data.length === 0)) {
+    //   this.renderError();
+    //   return;
+    // }
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    //converting newMarkup into a dom object
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+
+    const newElements = Array.from(newDOM.querySelectorAll("*"));
+    const currentElement = Array.from(
+      this._ParentElement.querySelectorAll("*")
+    );
+
+    // console.log(currentElement);
+    // console.log(newElements);
+
+    newElements.forEach((newEl, i) => {
+      const curEl = currentElement[i];
+
+      //update changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ""
+      ) {
+        // console.log("🔥", newEl.firstChild.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+
+      if (!newEl.isEqualNode(curEl)) {
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+      }
+    });
   }
 
   _clear() {
