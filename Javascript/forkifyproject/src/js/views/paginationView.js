8625f9aa6c62d3b461b4ce56602defac36a020c2
Brian Michael <brianmichaeladero@gmail.com>
2024-03-22 08:21:03 +0300

    pagination view

--- Javascript/forkifyproject/src/js/views/paginationView.js
+++ Javascript/forkifyproject/src/js/views/paginationView.js
@@ -6,28 +6,78 @@ class PaginationView extends View {
   _ParentElement = document.querySelector(".pagination");
 
   _generateMarkup() {
-    markup = `
-        <button class="btn--inline pagination__btn--prev">
-            <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-left"></use>
-            </svg>
-            <span>Page 1</span>
-        </button>
-        <button class="btn--inline pagination__btn--next">
-            <span>Page 3</span>
-            <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
-            </svg>
-        </button>
-    `;
+    const currentPage = this._data.page;
+
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    console.log(numPages);
 
     // page 1 and there are other pages
-
-    // page 1 and there are no other pages
+    if (currentPage === 1 && numPages > 1) {
+      return `
+          <button data-goto="${
+            currentPage + 1
+          }" class="btn--inline pagination__btn--next">
+            <span>Page ${currentPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </button>
+      `;
+    }
 
     //last page
+    if (currentPage === numPages && numPages > 1) {
+      return `
+          <button data-goto="${
+            currentPage - 1
+          }" class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${currentPage - 1}</span>
+          </button>
+      `;
+    }
 
     //other page
+    if (currentPage < numPages) {
+      return `
+          <button data-goto="${
+            currentPage - 1
+          }" class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${currentPage - 1}</span>
+          </button>
+          <button data-goto="${
+            currentPage + 1
+          }" class="btn--inline pagination__btn--next">
+            <span>Page ${currentPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </button>
+      `;
+    }
+
+    // page 1 and there are no other pages
+    return "";
+  }
+
+  //*function is for click on the pagination buttons
+  addHandlerClick(handler) {
+    this._ParentElement.addEventListener("click", function (e) {
+      e.preventDefault();
+      const btn = e.target.closest(".btn--inline");
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      console.log(goToPage);
+      handler(goToPage);
+    });
   }
 }
 
