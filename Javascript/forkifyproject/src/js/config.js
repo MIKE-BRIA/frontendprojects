314def404d1c3363f9faa883769eab968071732d
Brian Michael <brianmichaeladero@gmail.com>
2024-03-22 08:19:20 +0300

    config

--- Javascript/forkifyproject/src/js/config.js
+++ Javascript/forkifyproject/src/js/config.js
@@ -1,4 +1,4 @@
 //! contains constants that are used severally within the application
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
 export const TIMEOUT_SECONDS = 20;
-export const RES_PER_PAGE = 10;
+export const RES_PER_PAGE = 11;
